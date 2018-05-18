ea6a8b2c5e8d535023013f1b3b12c4c6d796479d
Michael COLL <mick.coll@gmail.com>
2018-05-18 22:48:24 +0200

    Fix database init script.

--- services/common-service/src/main/resources/db/migration/V1_0__Base-script.sql
+++ services/common-service/src/main/resources/db/migration/V1_0__Base-script.sql
@@ -2,4 +2,10 @@ CREATE TABLE `partenaire` (
   `ptn_id` int(11) NOT NULL AUTO_INCREMENT,
   `ptn_name` varchar(255) NOT NULL,
   PRIMARY KEY (`ptn_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `creche` (
+  `cre_id` int(11) NOT NULL AUTO_INCREMENT,
+  `cre_name` varchar(255) NOT NULL,
+  PRIMARY KEY (`cre_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8
